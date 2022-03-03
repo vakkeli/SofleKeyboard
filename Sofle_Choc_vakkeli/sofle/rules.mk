74e8d71e169fbeb4a574345e21c14f4583d95e31
Valtteri <valtteri.marttila@xiphera.com>
2022-03-03 09:11:51 +0200

    Fixes to pcb. Added functioning, but incomplete fw

---
+++ Sofle_Choc_vakkeli/sofle/rules.mk
@@ -0,0 +1,9 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+SPLIT_KEYBOARD = yes
+DEFAULT_FOLDER = sofle/vakkeli
+SWAP_HANDS_ENABLE = yes
---
+++ Sofle_Choc_vakkeli/sofle/sofle.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#if defined(KEYBOARD_sofle_rev1)
+#    include "rev1.h"
+#elif defined(KEYBOARD_sofle_keyhive)
+#    include "keyhive.h"
+#elif defined(KEYBOARD_sofle_vakkeli)
+#    include "vakkeli.h"
+#endif
